c3153ec47e78e665ffdefe84f7d5d7a529537479
anthonytorlucci <anthonytorlucci@gmail.com>
2022-12-17 17:05:06 -0600

    matplotlib colormaps

---
+++ mpl_summer.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+from colormappy.utils import matplotlib_linsegcmap_to_rgba
+from plotting import colormappy_figure, colormappy_attributes, scatter3D_Jpapbp, surface_sombrero
+
+mappy_cmap = cm.get_cmap(name='summer')
+mappy_rgb = matplotlib_linsegcmap_to_rgba(mappy_cmap, 256, False)
+
+mappy_name = 'matplotlib summer'
+fig = colormappy_figure(mappy_rgb, color_name=mappy_name)
+fig = colormappy_attributes(mappy_rgb, color_name=mappy_name)
+fig = scatter3D_Jpapbp(mappy_rgb, color_name=mappy_name)
+fig = surface_sombrero(mappy_rgb, color_name=mappy_name)
+
+plt.show()
