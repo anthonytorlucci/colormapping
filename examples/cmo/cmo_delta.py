0d95f2f6da14501710909d876fbb0ab0c9d90e56
anthonytorlucci <anthonytorlucci@gmail.com>
2022-12-20 06:19:15 -0600

    examples add context

---
+++ examples/cmo/cmo_delta.py
@@ -0,0 +1,16 @@
+from cmo_context import colormappy
+
+import matplotlib.pyplot as plt
+import cmocean
+
+from colormappy.utils import matplotlib_linsegcmap_to_rgba
+from colormappy_plotting import colormappy_figure, colormappy_attributes, scatter3D_Jpapbp, surface_sombrero
+
+mappy_rgb = matplotlib_linsegcmap_to_rgba(cmocean.cm.delta, 256, False)
+mappy_name = 'cmocean delta'
+fig = colormappy_figure(mappy_rgb, color_name=mappy_name)
+fig = colormappy_attributes(mappy_rgb, color_name=mappy_name)
+fig = scatter3D_Jpapbp(mappy_rgb, color_name=mappy_name)
+fig = surface_sombrero(mappy_rgb, color_name=mappy_name)
+
+plt.show()
--- examples/mpl/mpl_Paired.py
+++ examples/mpl/mpl_Paired.py
@@ -1,8 +1,10 @@
+from mpl_context import colormappy
+
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
 from colormappy.utils import matplotlib_linsegcmap_to_rgba
-from plotting import colormappy_figure, colormappy_attributes, scatter3D_Jpapbp, surface_sombrero
+from colormappy_plotting import colormappy_figure, colormappy_attributes, scatter3D_Jpapbp, surface_sombrero
 
 mappy_cmap = cm.get_cmap(name='Paired')
 mappy_rgb = matplotlib_linsegcmap_to_rgba(mappy_cmap, 256, False)
